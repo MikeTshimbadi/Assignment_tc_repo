a4e13ead558716c4970403cac75abc2465501263
MikeTshimbadi <Mike.Tshimbadi@techconsulting.tech>
2025-07-23 08:36:11 -0400

    New apps added

--- CountryApp/CountryAppTests/CountryAppTests.swift
+++ CountryApp/CountryAppTests/CountryAppTests.swift
@@ -10,27 +10,35 @@ import XCTest
 
 final class CountryAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var country: [Country] = []
+    
+    override func setUp() {
+        super.setUp()
+        // Initialize the country array with sample data
+        country = [
+            Country(
+                capital: "Paris ",
+                code: "France",
+                currency: Currency(code: "Eu", name: "Euro", symbol: "€"),
+                flag: "🇫🇷",
+                language: Language(code: "fr", name: "French"),
+                name: "France",
+                region: "EU"),
+            Country(
+                capital: "Washigthon D.C",
+                code: "USA",
+                currency: Currency(code: "USD", name: "US Dollar", symbol: "$"),
+                flag: "🇺🇸",
+                language: Language(code: "en", name: "English"),
+                name: "United Stated of America",
+                region: "NA"),
+                    ]
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testnumberOfCountries() {
+        let numberOfCountries = country.count
+        
+        XCTAssertEqual(numberOfCountries, 2, "The number of countries should be 2")
     }
 
 }
